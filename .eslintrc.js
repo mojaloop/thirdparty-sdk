10bf62fdfd12e3f1c8e6cbfc4787f4b49cfa9609
Paweł Marzec <pm@eoln.eu>
2020-07-29 13:41:49 +0200

    lint: drop commented rules

--- .eslintrc.js
+++ .eslintrc.js
@@ -9,8 +9,6 @@ module.exports = {
     'standard',
     'plugin:@typescript-eslint/eslint-recommended',
     'plugin:@typescript-eslint/recommended',    
-    //'prettier/@typescript-eslint', // Uses eslint-config-prettier to disable ESLint rules from @typescript-eslint/eslint-plugin that would conflict with prettier
-    //'plugin:prettier/recommended', // Enables eslint-plugin-prettier and displays prettier errors as ESLint errors. Make sure this is always the last configuration in the extends array.
     // Enforces ES6+ import/export syntax
     'plugin:import/errors',
     'plugin:import/warnings',
