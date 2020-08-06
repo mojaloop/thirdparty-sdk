8ecbe58755a211b0308601523ad1c311f462e0c3
Paweł Marzec <pm@eoln.eu>
2020-08-06 15:48:42 +0200

    lint: update config

--- .eslintrc.js
+++ .eslintrc.js
@@ -17,6 +17,8 @@ module.exports = {
   parserOptions: {
     ecmaVersion: 2018, // Allows for the parsing of modern ECMAScript features
     sourceType: 'module', // Allows for the use of imports
+    project: "./tsconfig.json",
+    tsConfigRootDir: "./"
   },
   rules: {
     indent: 'off',
