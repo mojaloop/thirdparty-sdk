d30fac4a807cf4cd21a177d47cc31fa9c1ffa1ec
Paweł Marzec <pm@eoln.eu>
2020-08-19 10:59:16 +0200

    feat(shared/pub-sub): first draft

---
+++ src/shared/pub-sub.ts
@@ -0,0 +1,141 @@
+/*****
+ License
+ --------------
+ Copyright © 2020 Mojaloop Foundation
+ The Mojaloop files are made available by the Mojaloop Foundation under the Apache License, Version 2.0 (the "License")
+ and you may not use these files except in compliance with the License. You may obtain a copy of the License at
+ http://www.apache.org/licenses/LICENSE-2.0
+ Unless required by applicable law or agreed to in writing, the Mojaloop files are distributed
+ on an "AS IS" BASIS, WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ See the License for the specific language governing permissions and limitations under the License.
+ Contributors
+ --------------
+ This is the official list of the Mojaloop project contributors for this file.
+ Names of the original copyright holders (individuals or organizations)
+ should be listed with a '*' in the first column. People who have
+ contributed from an organization can be listed under the organization
+ that actually holds the copyright for their contributions (see the
+ Gates Foundation organization for an example). Those individuals should have
+ their names indented and be marked with a '-'. Email address can be added
+ optionally within square brackets <email>.
+ * Gates Foundation
+ - Name Surname <name.surname@gatesfoundation.com>
+
+ - Mob
+ --------------
+ ******/
+
+import { RedisConnection } from './redis-connection'
+import { promisify } from 'util'
+
+export class InvalidCallbackIdError extends Error {
+  constructor () {
+    super('valid callbackId must be positive number')
+  }
+
+  // validation logic for callbackId parameter
+  static throwIfInvalid (callbackId: number): void {
+    if (!(callbackId > 0)) {
+      throw new InvalidCallbackIdError()
+    }
+  }
+}
+
+export class InvalidChannelNameError extends Error {
+  constructor () {
+    super('channel name must be non empty string')
+  }
+
+  // validation logic for channel name parameter
+  static throwIfInvalid (channel: string): void {
+    if (!(channel?.length)) {
+      throw new InvalidChannelNameError()
+    }
+  }
+}
+
+export type Message = Record<string, unknown>
+export type NotificationCallback = (channel: string, message: Message, id: number) => void
+
+export class PubSub extends RedisConnection {
+  private callbacks = new Map<string, Map<number, NotificationCallback>>()
+  private callbackId = 0
+
+  async connect (): Promise<void> {
+    await super.connect()
+    this.client.on('message', this.broadcastNotification.bind(this))
+  }
+
+  protected broadcastNotification (channel: string, stringified: string): void {
+    // deserialize message
+    const message: Message = JSON.parse(stringified)
+
+    const callbacksForChannel = this.callbacks.get(channel)
+
+    // do nothing if channel doesn't exist
+    if (!callbacksForChannel) {
+      return
+    }
+
+    // broadcast message by calling all callbacks
+    callbacksForChannel.forEach(
+      (callback: NotificationCallback, id: number): void => callback(channel, message, id)
+    )
+  }
+
+  get nextCallbackId (): number {
+    return ++this.callbackId
+  }
+
+  subscribe (channel: string, callback: NotificationCallback): number {
+    InvalidChannelNameError.throwIfInvalid(channel)
+
+    // is callbacks map for channel present
+    if (!this.callbacks.has(channel)) {
+      // initialize the channel callbacks map
+      this.callbacks.set(channel, new Map<number, NotificationCallback>())
+
+      // only once time subscribe to Redis channel
+      this.client.subscribe(channel)
+    }
+
+    const callbacksForChannel = this.callbacks.get(channel)
+
+    // allocate new id for callback
+    const id = this.nextCallbackId
+
+    // register callback for given channel
+    callbacksForChannel?.set(id, callback)
+
+    // return registration id to be used by unsubscribe method
+    return id
+  }
+
+  unsubscribe (channel: string, callbackId: number): void {
+    // input parameters validation
+    InvalidChannelNameError.throwIfInvalid(channel)
+    InvalidCallbackIdError.throwIfInvalid(callbackId)
+
+    // do nothing if there is no channel
+    const callbacksForChannel = this.callbacks.get(channel)
+    if (!callbacksForChannel) {
+      return
+    }
+
+    // do nothing if there is no callback for registration id
+    if (!callbacksForChannel.has(callbackId)) {
+      return
+    }
+
+    // unregister callback
+    callbacksForChannel.delete(callbackId)
+  }
+
+  async publish (channel: string, message: Message): Promise<void> {
+    InvalidChannelNameError.throwIfInvalid(channel)
+
+    const jsonified = JSON.stringify(message)
+    const asyncPublish = promisify(this.client.publish)
+    await asyncPublish.call(this.client, channel, jsonified)
+  }
+}
