3bc5ec04194431c8bd91e67be7d375b810f8ed6d
Hernani Batista <chbhernani@users.noreply.github.com>
2020-10-07 08:56:22 -0300

    Update outbound-api.test.ts

--- test/unit/handlers/outbound-api.test.ts
+++ test/unit/handlers/outbound-api.test.ts
@@ -282,7 +282,6 @@ describe('Outbound API routes', (): void => {
     })
   })
 
-  // jest.setTimeout(30000)
   it('/thirdpartyRequests/transactions/{ID}/authorizations', async (): Promise<void> => {
     const request = {
       method: 'POST',
