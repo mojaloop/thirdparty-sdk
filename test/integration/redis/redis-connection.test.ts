1efc9b61b2dbdf72264bc3b5e3fc01d480d55ecc
Paweł Marzec <pm@eoln.eu>
2021-03-29 11:01:31 +0200

    fix: integration test for redis

--- test/integration/redis/redis-connection.test.ts
+++ test/integration/redis/redis-connection.test.ts
@@ -58,7 +58,7 @@ describe('RedisConnection', () => {
     invalidPort.port = 8080
     const invalidRC = new RedisConnection(invalidPort)
     expect(invalidRC.connect()).rejects.toThrowError(
-      new Error('Ready check failed: Fatal error encountered. Command aborted. It might have been processed.')
+      new Error('Redis connection to localhost:8080 failed - connect ECONNREFUSED 127.0.0.1:8080')
     )
     expect(invalidRC.isConnected).toBeFalsy()
   })
