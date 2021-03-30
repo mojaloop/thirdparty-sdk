8a4a89493ffcefe784d9bd678f0cd40198aad875
Kevin Leyow <kleyow@gmail.com>
2021-03-30 10:51:56 -0500

    chore: regenerate interfaces

--- src/interface/outbound/api_interfaces/openapi.d.ts
+++ src/interface/outbound/api_interfaces/openapi.d.ts
@@ -819,6 +819,12 @@ export interface components {
       toParticipantId: string;
       authToken: string;
     };
+    /** State of POST consent requests validate */
+    ConsentRequestsValidateState: string;
+    ConsentRequestsValidateResponseError: {
+      errorInformation: components['schemas']['ErrorInformation'];
+      currentState: components['schemas']['ConsentRequestsValidateState'];
+    };
     /**
      * The scopes requested for a ConsentRequest.
      * - "accounts.getBalance" - Get the balance of a given account.
@@ -845,8 +851,13 @@ export interface components {
       consentRequestId: components['schemas']['CorrelationId'];
       scopes: components['schemas']['Scope'][];
     };
-    /** State of POST consent requests validate */
-    ConsentRequestsValidateState: string;
+    ConsentRequestsValidateResponseSuccess: {
+      consent: components['schemas']['ConsentsPostRequest'];
+      currentState: components['schemas']['ConsentRequestsValidateState'];
+    };
+    ConsentRequestsValidateResponse:
+    | components['schemas']['ConsentRequestsValidateResponseError']
+    | components['schemas']['ConsentRequestsValidateResponseSuccess'];
   };
   responses: {
     /** OK */
@@ -980,10 +991,7 @@ export interface components {
     /** Consent requests validate response */
     ConsentRequestsValidateResponse: {
       content: {
-        'application/json': {
-          consent?: components['schemas']['ConsentsPostRequest'];
-          currentState?: components['schemas']['ConsentRequestsValidateState'];
-        };
+        'application/json': components['schemas']['ConsentRequestsValidateResponse'];
       };
     };
   };
