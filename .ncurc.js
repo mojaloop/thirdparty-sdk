e9e0f6a78e238df73a31ab5c7d40652ce7bb48bd
Kevin <kleyow@gmail.com>
2022-05-23 22:52:51 -0500

    chore: comments

--- .ncurc.js
+++ .ncurc.js
@@ -1,14 +1,14 @@
 module.exports = {
   reject: [
-    // husky >4 involves a full config migration with no current and apparent benefit.
+    // Upgrading past husky@4 to involves a full config migration with no current and apparent benefit.
     // So we are just sticking to husky@4.x.x for the time being.
     'husky',
     // Caution advised in upgrading redis-mock past 0.52.0. Investigation needed.
     'redis-mock',
-    // Upgrading redis >3 the the next major version has a lot of breaking changes.
+    // Upgrading past redis@3 to the next major version introduces a lot of breaking changes.
     'redis',
     '@types/redis',
-    // jest/ts-jest >26 introduces a lot of breaking changes to current tests.
+    // Upgrading past jest/ts-jest@26 introduces a lot of breaking changes to current tests.
     'jest',
     'ts-jest'
   ]
