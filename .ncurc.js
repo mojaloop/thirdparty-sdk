1a803267965b9dbb683be077e174c7b9a5f4ec59
Shashikant Hirugade <shashi.pioneer@gmail.com>
2025-02-03 10:56:24 -0600

    chore: update dependencies

--- .ncurc.js
+++ .ncurc.js
@@ -6,6 +6,8 @@ module.exports = {
     'redis',
     '@types/redis',
     // Upgrading past commander@7 introduces a lot of breaking changes.
-    'commander'
+    'commander',
+    // Need a major refactor to upgrade to the next major version of this package.
+    '@mojaloop/sdk-scheme-adapter'
   ]
 }
